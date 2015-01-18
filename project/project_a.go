64c45ecb3e3b87b7185292d96e480f7176ef234d
leoyang <milliyang@sina.cn>
2015-01-18 22:55:56 +0800

    update strategy

--- project/project_a.go
+++ project/project_a.go
@@ -12,12 +12,17 @@ const (
 
 func init() {
 	if debug {
-		fmt.Println("sample.quant.project.init")
+		fmt.Println("quanta.project.init")
 	}
 
 	proj := quant.NewProject()
 	proj.Name = "ProjectA"
 
+	proj.AddInstrument("s000001")
+	proj.AddInstrument("s000002")
+	proj.AddInstrument("s000003")
+	proj.AddInstrument("s000004")
+	proj.AddInstrument("s000005")
+	proj.AddInstrument("s000006")
 	proj.Strategy(strategy.MyStrategy{})
-
 }
--- strategy/mystrategy.go
+++ strategy/mystrategy.go
@@ -12,7 +12,7 @@ const (
 
 func init() {
 	if debug {
-		fmt.Println("sample.quant.strategy.init")
+		fmt.Println("quanta.strategy package init")
 	}
 }
 
@@ -20,13 +20,16 @@ type MyStrategy struct {
 	strategy.Strategy
 }
 
-func (this *MyStrategy) Init() {
-	this.Strategy.Init()
-	this.Name = "Strategy"
+func (this *MyStrategy) Init(symbol string) {
+	if debug {
+		fmt.Println("MyStrategy.Init()")
+	}
+	this.Strategy.Init(symbol)
+	this.Name = "MyStrategy"
 }
 
 func (this *MyStrategy) OnStrategyStart() {
-	fmt.Println("MyStrategy", "OnStrategyStart")
+	fmt.Println("MyStrategy", "OnStrategyStart", "Name", this.Name, "Symbol:", this.Symbol)
 }
 
 func (this *MyStrategy) OnStrategyStop() {
