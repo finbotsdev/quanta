2a780f4a7fa2a1b48993c8c5d01beafd2c6a4954
leoyang <milliyang@sina.cn>
2015-01-19 23:24:06 +0800

    Go Bug?  [GoBug Tag00001] update

--- main.go
+++ main.go
@@ -23,17 +23,12 @@ func main() {
 
 	sma := indicator.NewSMA(fSeries, 6)
 
-	// var iseries series.ISeries = sma
-
 	now := time.Now()
 	var i64 float64
 	for i := 0; i < 10; i++ {
 		i64 = float64(i + 1)
-
 		now = now.Add(time.Second)
 		fSeries.Append(&now, i64)
-		// sma.Append(&now, i64)
-		// iseries.Append(&now, i64)
 	}
 
 	// fmt.Println(sma.Keys())
