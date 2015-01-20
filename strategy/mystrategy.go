cf7f02c9efae47d8afced25432647d6fe59b4b12
leoyang <milliyang@sina.cn>
2015-01-21 01:09:59 +0800

    add EMA

--- strategy/mystrategy.go
+++ strategy/mystrategy.go
@@ -22,7 +22,9 @@ func init() {
 type MyStrategy struct {
 	strategy.Strategy
 
-	sma *indicator.SMA
+	sma   *indicator.SMA
+	ema16 *indicator.EMA
+	ema26 *indicator.EMA
 }
 
 func (this *MyStrategy) Init(symbol string, barSeries *bar.BarSeries) {
@@ -40,6 +42,8 @@ func (this *MyStrategy) OnStrategyStart() {
 
 	if test_bar_series_and_sma {
 		this.sma = indicator.NewSMA(this.BarSeries, 6)
+		this.ema16 = indicator.NewEMA(this.BarSeries, 16)
+		this.ema26 = indicator.NewEMA(this.BarSeries, 26)
 	}
 }
 
@@ -49,6 +53,9 @@ func (this *MyStrategy) OnStrategyStop() {
 	if test_bar_series_and_sma {
 		printslice("base:", this.BarSeries.Values())
 		printslice("sma:", this.sma.Values())
+		printslice("ema16:", this.ema16.Values())
+		printslice("ema26:", this.ema26.Values())
+
 	}
 }
 
@@ -57,7 +64,9 @@ func (this *MyStrategy) OnBar(bar_ bar.Bar) {
 		fmt.Println("MyStrategy", this.Symbol, "OnBar")
 	}
 
-	now := this.BarSeries.Now()
+	// now := this.BarSeries.Now()
+	now := bar_.DateTime
+
 	if !this.sma.IsFake(&now) {
 		if this.sma.ValueAtTime(&now) > this.BarSeries.ValueAtTime(&now) {
 			fmt.Print("sma > base: Buy\n")
