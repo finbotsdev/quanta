8c1bf9713714a0294a32eedd319e1d820dc13a5e
leoyang <milliyang@sina.cn>
2015-01-30 00:11:13 +0800

    add draw line

--- strategy/mystrategy.go
+++ strategy/mystrategy.go
@@ -3,6 +3,7 @@ package strategy
 import (
 	"fmt"
 	"quant/base/bar"
+	"quant/base/color"
 	"quant/base/indicator"
 	"quant/base/series"
 	"quant/base/strategy"
@@ -23,10 +24,11 @@ func init() {
 type MyStrategy struct {
 	strategy.Strategy
 
-	sma   *indicator.SMA
-	ema16 *indicator.EMA
-	ema26 *indicator.EMA
-	macd  *indicator.MACD
+	sma       *indicator.SMA
+	ema16     *indicator.EMA
+	ema26     *indicator.EMA
+	macd      *indicator.MACD
+	closeLine *series.FloatSeries
 }
 
 func (this *MyStrategy) Init(symbol string, barSeries *series.BarSeries) {
@@ -43,23 +45,33 @@ func (this *MyStrategy) OnStrategyStart() {
 	fmt.Println("MyStrategy", "OnStrategyStart", "Name", this.Name, "Symbol:", this.Symbol)
 
 	if test_bar_series_and_sma {
-		this.sma = indicator.NewSMA(this.BarSeries, 6)
+
+		this.closeLine = series.NewFloatSeries()
+		this.closeLine.InitWorkaround(this.BarSeries, color.Indigo)
+
+		this.sma = indicator.NewSMA(this.BarSeries, 3)
+		this.sma.Color = color.Green
 		this.ema16 = indicator.NewEMA(this.BarSeries, 16)
+		this.ema16.Color = color.Gold
 		this.ema26 = indicator.NewEMA(this.BarSeries, 26)
 		this.macd = indicator.NewMACD(this.BarSeries, 16, 26, 9)
 	}
 
 	this.Draw(0, this.BarSeries)
+	this.Draw(0, this.closeLine)
 	this.Draw(0, this.sma)
 	this.Draw(0, this.ema16)
-	this.Draw(0, this.ema26)
-	this.Draw(0, this.macd)
+	// this.Draw(0, this.ema26)
+	// this.Draw(0, this.macd)
+
+	this.Draw(1, this.BarSeries)
+	this.Draw(1, this.closeLine)
 }
 
 func (this *MyStrategy) OnStrategyStop() {
 	fmt.Println("MyStrategy", "OnStrategyStop", "Name", this.Name, "Symbol:", this.Symbol)
 
-	if test_bar_series_and_sma {
+	if false && test_bar_series_and_sma {
 		printslice("base:", this.BarSeries.Values())
 		printslice("sma:", this.sma.Values())
 		printslice("ema16:", this.ema16.Values())
